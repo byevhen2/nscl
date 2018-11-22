d7ec3a8e7726c7b7115439b2a48f57ae645b5c24
Yevhen Bilyavskyi <kapai1890@gmail.com>
2018-11-22 22:53:25 +0200

    Add more validation functions

--- PHP7/validation.php
+++ PHP7/validation.php
@@ -14,3 +14,103 @@ if (!function_exists('is_valid_url')) {
         return (filter_var($url, FILTER_VALIDATE_URL) !== false);
     }
 }
+
+if (!function_exists('validate_bool')) {
+    function validate_bool($raw): bool
+    {
+        return filter_var($raw, FILTER_VALIDATE_BOOLEAN);
+    }
+}
+
+if (!function_exists('validate_id')) {
+    /**
+     * @param mixed $raw The value to convert to ID.
+     * @return int The value in range [0; oo).
+     */
+    function validate_id($raw): int
+    {
+        return validate_int($raw, 0);
+    }
+}
+
+if (!function_exists('validate_ids')) {
+    /**
+     * @param string|array $raw Comma-separated IDs or an array of IDs.
+     * @return array An array of valid IDs (valid - means "no zeros").
+     */
+    function validate_ids($raw): array
+    {
+        if (!is_array($raw)) {
+            $raw = explode(',', $raw);
+        }
+
+        $ids = [];
+
+        foreach ($raw as $rawId) {
+            $id = validate_id($rawId);
+
+            // Filter empty values
+            if ($id > 0) {
+                $ids[] = $id;
+            }
+        }
+
+        return $ids;
+    }
+}
+
+if (!function_exists('validate_int')) {
+    /**
+     * @param mixed $raw The value to convert to integer.
+     * @param int $min Optional. -Infinity by default.
+     * @param int $max Optional. Infinity by default.
+     * @return int The value in range [$min; $max].
+     */
+    function validate_int($raw, int $min = null, int $max = null): int
+    {
+        // Also convert false (on failure) into int
+        $value = (int)filter_var($raw, FILTER_VALIDATE_INT);
+
+        $min = ($min ?? $value);
+        $max = ($max ?? $value);
+
+        // $min <= $value <= $max
+        return max($min, min($value, $max));
+    }
+}
+
+if (!function_exists('validate_order')) {
+    /**
+     * @param mixed $raw The value to validate.
+     * @param string $default Optional. Default order. "DEST" by default.
+     * @return string "DEST"|"ASC"
+     */
+    function validate_order($value, string $default = 'DEST'): string
+    {
+        $value = strtoupper($raw);
+
+        if ($value == 'DEST' || $value == 'ASC') {
+            return $value;
+        } else {
+            return $default;
+        }
+    }
+}
+
+if (!function_exists('validate_relation')) {
+    /**
+     * @param mixed $raw The value to validate.
+     * @param string $default Optional. Default relation. "OR" by default.
+     * @return string "OR"|"AND"
+     */
+    function validate_relation($raw, string $default = 'OR'): string
+    {
+        $value = strtoupper($raw);
+
+        if ($value == 'OR' || $value == 'AND') {
+            return $value;
+        } else {
+            return $default;
+        }
+    }
+}
